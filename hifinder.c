5d564ba70655b6354bae4cf3a1f3937ba9c8560a
Awsaf <awsaf.sakif2@gmail.com>
2016-11-03 18:26:13 +0000

    producing correct file path

--- hifinder.c
+++ hifinder.c
@@ -8,16 +8,23 @@
 #include <string.h>
 
 int main(int argc, char **argv) {
-    FILE *ptr_file;
-    char text[100];
-    int i =0;
-    char buf[200];
+    int i;
+    
+    
     char dest_init[] = "/sys/class/hidraw/hidraw";
     char dest_final[] = "/device/modalias";
+     
+    for(i=0; i<3; i = i + 1) {
+    FILE *ptr_file;
     char dest[100];
+    char buf[200];
+    char str[1];
+    char text[100];
+    sprintf(str, "%d", i);
     strcat(dest, dest_init);
-    strcat(dest, "1");
+    strcat(dest, str);
     strcat(dest, dest_final);
+    
     ptr_file = fopen(dest, "r");
 
     if (ptr_file != NULL) {
@@ -25,6 +32,7 @@ int main(int argc, char **argv) {
 
         if(strstr(text, "6604") != NULL) {
             snprintf(buf, sizeof(buf), "echo HID we want %s", text);
+            break;
         }
         else {
             snprintf(buf, sizeof(buf), "echo Not the HID we want %s", text);
@@ -32,6 +40,17 @@ int main(int argc, char **argv) {
 
         system(buf);
         fclose(ptr_file);
+        }
+    dest[0] = '\0';
     }
+    char file_path[100];
+    char print_path[100];
+    char destination[] = "/dev/hidraw";
+    strcat(file_path, destination);
+    char hid_no[1];
+    sprintf(hid_no, "%d", i);
+    strcat(file_path, hid_no);
+    snprintf(print_path, sizeof(print_path), "echo file path: %s", file_path);
+    system(print_path);    
     return 0;
 }
